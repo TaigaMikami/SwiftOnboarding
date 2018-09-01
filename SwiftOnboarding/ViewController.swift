076a43629483d0f955acad8ed946696f03200867
TaigaMikami <mikamimikami60@gmail.com>
2018-09-02 00:44:02 +0900

    Initial Commit

---
+++ SwiftOnboarding/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftOnboarding
+//
+//  Created by 三上大河 on 2018/09/02.
+//  Copyright © 2018年 三上大河. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
